33a0c846b099cba4b3f09b1e0a1fde4090fc941a
Salvatorenodes <102610998+Salvatorenodes@users.noreply.github.com>
2023-03-14 19:25:58 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -46,23 +46,3 @@ git clone https://github.com/AleoHQ/leo
 sleep 1
 cd leo
 cargo install --path .
-sleep 1
-cd "$OLDPWD"
-
-mkdir demo_deploy_Leo_app && cd demo_deploy_Leo_app
-
-read -p "Enter your wallet address: " WALL
-WALLETADDRESS="$WALL"
-
-APPNAME=helloworld_"${WALLETADDRESS:4:6}"
-leo new "${APPNAME}"
-cd "${APPNAME}" && leo run && cd -
-PATHTOAPP=$(realpath -q $APPNAME)
-cd $PATHTOAPP && cd ..
-
-read -p "Enter your private key: " PRKEY
-PRIVATEKEY="$PRKEY"
-
-RECORD="$(cat)"
-
-snarkos developer deploy "${APPNAME}.aleo" --private-key "${PRIVATEKEY}" --query "https://vm.aleo.org/api" --path "./${APPNAME}/build/" --broadcast "https://vm.aleo.org/api/testnet3/transaction/broadcast" --fee 600000 --record "${RECORD}"

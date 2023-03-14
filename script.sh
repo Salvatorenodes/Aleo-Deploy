4baf6555c7f9d5c5f867dafd26d8d47140842887
Salvatorenodes <102610998+Salvatorenodes@users.noreply.github.com>
2023-03-14 11:34:11 +0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+echo "Powered by"
+curl -s https://raw.githubusercontent.com/Salvatorenodes/logo/main/logo.sh
+
+sleep 2
+
+echo "Updating and installing packages"
+sudo apt update && sudo apt upgrade -y
+sleep 1
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sleep 1
+git clone https://github.com/AleoHQ/snarkOS.git --depth 1
+cd snarkOS
+sleep 1
+./build_ubuntu.sh
+sleep 1
+source $HOME/.cargo/env
+sleep 1
+cargo install --path .
+sleep 1
+cd -

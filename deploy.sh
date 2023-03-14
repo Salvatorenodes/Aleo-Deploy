7009870a6b803370aeaa46fb0333359c26a7ed6e
Salvatorenodes <102610998+Salvatorenodes@users.noreply.github.com>
2023-03-14 20:30:31 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+mkdir demo_deploy_Leo_app && cd demo_deploy_Leo_app
+sleep1
 read -p "Enter your repository name: " REPO
 cd /workspace/$REPO
 sleep 1
 rm -r demo_deploy_Leo_app
 sleep 1
-
 read -p "Enter your wallet address: " WAL
 WALLETADDRESS="$WAL"
 sleep 1

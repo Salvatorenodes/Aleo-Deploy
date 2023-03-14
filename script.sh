13212b4576dcf57c16f973248f958fb383ecbe04
Salvatorenodes <102610998+Salvatorenodes@users.noreply.github.com>
2023-03-14 11:44:40 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,12 +8,7 @@ sleep 2
 echo "Updating and installing packages"
 sudo apt update && sudo apt upgrade -y
 sleep 1
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh | expect <<EOF
-  spawn curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-  sleep 1
-  expect "Current installation options" { send -- "1\r" }
-  
-  
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 sleep 1
 git clone https://github.com/AleoHQ/snarkOS.git --depth 1
 cd snarkOS

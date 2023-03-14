de410ead21b69c1a5e40052b71791acf478c0ecf
Salvatorenodes <102610998+Salvatorenodes@users.noreply.github.com>
2023-03-14 17:45:39 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,6 +12,7 @@ sleep 1
 read -p "Enter your repository name: " EXAMPLE
 export RUSTUP_HOME=/workspace/$EXAMPLE
 
+echo "Install rustup"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 sleep 1
@@ -20,6 +21,7 @@ source "/workspace/.cargo/env"
 
 sleep 1
 
+echo "Create snarkOS"
 git clone https://github.com/AleoHQ/snarkOS.git --depth 1
 sleep 1
 sudo chown -R gitpod ./snarkOS
@@ -39,3 +41,20 @@ cd leo
 cargo install --path .
 sleep1
 cd "$OLDPWD"
+
+mkdir demo_deploy_Leo_app && cd demo_deploy_Leo_app
+
+echo "Enter your wallet address"
+WALLETADDRESS="$(cat)"
+
+APPNAME=helloworld_"${WALLETADDRESS:4:6}"
+leo new "${APPNAME}"
+cd "${APPNAME}" && leo run && cd -
+PATHTOAPP=$(realpath -q $APPNAME)
+cd $PATHTOAPP && cd ..
+
+PRIVATEKEY="$(cat)"
+
+RECORD="$(cat)"
+
+snarkos developer deploy "${APPNAME}.aleo" --private-key "${PRIVATEKEY}" --query "https://vm.aleo.org/api" --path "./${APPNAME}/build/" --broadcast "https://vm.aleo.org/api/testnet3/transaction/broadcast" --fee 600000 --record "${RECORD}"
